f77d6521cfbd2bfae4243cbd86210adbbf56137e
Daniel Cruz <nox.sb@hotmail.com>
2022-10-23 22:09:03 -0500

    Starter files

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+
+const koders = [
+  {
+    id: "1",
+    name: "Rodrigo",
+    email: "rodrgio@kodemia.com",
+    age: 20,
+    generation: 21,
+  },
+  {
+    id: "2",
+    name: "Emanuel",
+    email: "emanuel@kodemia.com",
+    age: 20,
+    generation: 21,
+  },
+  {
+    id: "3",
+    name: "Héctor",
+    email: "hector@kodemia.com",
+    age: 20,
+    generation: 21,
+  },
+];
+
+function App() {
+  return (
+    <main className="min-h-screen bg-gray-300">
+      <div className="flex flex-col items-center text-center space-y-4 pt-10 text-lg w-[400px] mx-auto">
+        <form className="flex flex-col text-left w-full space-y-4">
+          <label className="flex justify-between">
+            Nombre
+            <input type="text" />
+          </label>
+          <label className="flex justify-between">
+            Correo
+            <input type="text" />
+          </label>
+          <label className="flex justify-between">
+            Edad
+            <input type="number" />
+          </label>
+          <label className="flex justify-between">
+            Generación
+            <select name="" id="">
+              <option value="22">22</option>
+              <option value="23">23</option>
+              <option value="24">24</option>
+            </select>
+          </label>
+          <input
+            className="bg-purple-500 rounded-md py-3 text-purple-900 cursor-pointer"
+            type="submit"
+            value="Añadir Koder"
+          />
+        </form>
+        <ul className="flex flex-col space-y-5 text-left">
+          {koders.map(({ id, name, email, age, generation }) => (
+            <li key={id}>
+              <article className="bg-white rounded-lg shadow px-4 py-3">
+                <p>
+                  <span className="font-bold">Nombre:</span> {name}
+                </p>
+                <p>
+                  <span className="font-bold"> Correo:</span> {email}
+                </p>
+                <p>
+                  <span className="font-bold">Edad:</span> {age}
+                </p>
+                <p>
+                  <span className="font-bold">Generación:</span> {generation}
+                </p>
+              </article>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+export default App;
